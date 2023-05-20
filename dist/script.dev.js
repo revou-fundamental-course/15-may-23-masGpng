66f73910712a8656d8cb9eca19d666ff613aa6cc
masGpng <134066009+masGpng@users.noreply.github.com>
2023-05-20 05:23:36 +0000

    final commit

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,6 +1,82 @@
 "use strict";
 
-var navItem = document.querySelector('.nav-item a');
-navItem.addEventListener('click', function (e) {
-  console.log(e.target.closest(".nav-item").classList.contains('active'));
+var tabLinks = document.querySelectorAll('.nav-item a');
+var tabContentParents = document.querySelectorAll('.tab-pane');
+tabLinks.forEach(function (tabLink, index) {
+  tabLink.addEventListener('click', function () {
+    tabLinks.forEach(function (link) {
+      return link.parentNode.classList.remove('active');
+    });
+    tabContentParents.forEach(function (parent) {
+      return parent.classList.remove('active');
+    });
+    tabLink.parentNode.classList.add('active');
+    tabContentParents[index].classList.add('active');
+  });
 });
+var sisiLuas, sisiKeliling, panjangLuas, lebarLuas, panjangKeliling, lebarKeliling;
+var outputLuasPersegi, outputKelilingPersegi, outputLuasPpanjang, outputKelilingPpanjang;
+var pPanjangInputValue = document.getElementsByName('array[]');
+console.log(pPanjangInputValue);
+
+var luasPersegi = function luasPersegi(s) {
+  console.log(s.value * s.value);
+  sisiLuas = s.value;
+  outputLuasPersegi = s.value * s.value;
+};
+
+var kelilingPersegi = function kelilingPersegi(s) {
+  console.log(4 * s.value);
+  sisiKeliling = s.value;
+  outputKelilingPersegi = 4 * s.value;
+};
+
+var hitungLuasPersegi = function hitungLuasPersegi(e) {
+  var output = e.previousElementSibling;
+  var outputTag = "\n    <p>L = ".concat(sisiLuas, " x ").concat(sisiLuas, "</p>\n    <p>L = ").concat(outputLuasPersegi, "</p>\n    ");
+  output.innerHTML = outputTag;
+};
+
+var hitungKelilingPersegi = function hitungKelilingPersegi(e) {
+  var output = e.previousElementSibling;
+  var outputTag = "\n    <p>K = 4 x ".concat(sisiKeliling, "</p>\n    <p>K = ").concat(outputKelilingPersegi, "</p>\n    ");
+  output.innerHTML = outputTag;
+};
+
+var getValueFromInputs = function getValueFromInputs(formId) {
+  var form = document.getElementById(formId);
+  var panjang = form.elements["panjang"].value;
+  var lebar = form.elements["lebar"].value;
+  return {
+    valPanjang: panjang,
+    valLebar: lebar
+  };
+};
+
+var luasPersegiPanjang = function luasPersegiPanjang() {
+  panjangLuas = getValueFromInputs('luasPersegiPanjang').valPanjang;
+  lebarLuas = getValueFromInputs('luasPersegiPanjang').valLebar;
+  outputLuasPpanjang = panjangLuas * lebarLuas;
+};
+
+var kelilingPersegiPanjang = function kelilingPersegiPanjang() {
+  panjangKeliling = getValueFromInputs('kelilingPersegiPanjang').valPanjang;
+  lebarKeliling = getValueFromInputs('kelilingPersegiPanjang').valLebar;
+  outputKelilingPpanjang = 2 * panjangKeliling + 2 * lebarKeliling;
+};
+
+var hitungLuasPersegiPanjang = function hitungLuasPersegiPanjang(e) {
+  var output = e.previousElementSibling;
+  var outputTag = "\n    <p>L = ".concat(panjangLuas, " x ").concat(lebarLuas, "</p>\n    <p>L = ").concat(outputLuasPpanjang, "</p>\n    ");
+  output.innerHTML = outputTag;
+};
+
+var hitungKelilingPersegiPanjang = function hitungKelilingPersegiPanjang(e) {
+  var output = e.previousElementSibling;
+  var outputTag = "\n    <p>K = 2 x (".concat(panjangKeliling, " + ").concat(lebarKeliling, ")</p>\n    <p>K = ").concat(outputKelilingPpanjang, "</p>\n    ");
+  output.innerHTML = outputTag;
+};
+
+var resetAll = function resetAll(e) {
+  e.closest('form').querySelector('.output').innerHTML = "";
+};
--- src/script.js
+++ src/script.js
@@ -1,5 +1,98 @@
-const navItem = document.querySelector('.nav-item a');
+const tabLinks = document.querySelectorAll('.nav-item a');
+const tabContentParents = document.querySelectorAll('.tab-pane');
 
-navItem.addEventListener('click', (e) => {
-    console.log(e.target.closest(".nav-item").classList.contains('active'));
+tabLinks.forEach((tabLink, index) => {
+    tabLink.addEventListener('click', () => {
+        tabLinks.forEach(link => link.parentNode.classList.remove('active'));
+        tabContentParents.forEach(parent => parent.classList.remove('active'));
+
+        tabLink.parentNode.classList.add('active');
+        tabContentParents[index].classList.add('active');
+    });
 });
+
+let sisiLuas, sisiKeliling, panjangLuas, lebarLuas, panjangKeliling, lebarKeliling;
+let outputLuasPersegi, outputKelilingPersegi, outputLuasPpanjang, outputKelilingPpanjang;
+
+const pPanjangInputValue = document.getElementsByName('array[]');
+
+console.log(pPanjangInputValue);
+
+const luasPersegi = (s) => {
+    console.log(s.value * s.value);
+    sisiLuas = s.value;
+    outputLuasPersegi = s.value * s.value;
+}
+
+const kelilingPersegi = (s) => {
+    console.log(4 * s.value);
+    sisiKeliling = s.value;
+    outputKelilingPersegi = 4 * s.value;
+}
+
+const hitungLuasPersegi = (e) => {
+    const output = e.previousElementSibling;
+    const outputTag = `
+    <p>L = ${sisiLuas} x ${sisiLuas}</p>
+    <p>L = ${outputLuasPersegi}</p>
+    `;
+
+    output.innerHTML = outputTag;
+}
+
+const hitungKelilingPersegi = (e) => {
+    const output = e.previousElementSibling;
+    const outputTag = `
+    <p>K = 4 x ${sisiKeliling}</p>
+    <p>K = ${outputKelilingPersegi}</p>
+    `;
+
+    output.innerHTML = outputTag;
+}
+
+const getValueFromInputs = (formId) => {
+    const form = document.getElementById(formId);
+    const panjang = form.elements["panjang"].value;
+    const lebar = form.elements["lebar"].value;
+
+    return {
+        valPanjang: panjang,
+        valLebar: lebar
+    }
+}
+
+const luasPersegiPanjang = () => {
+    panjangLuas = getValueFromInputs('luasPersegiPanjang').valPanjang;
+    lebarLuas = getValueFromInputs('luasPersegiPanjang').valLebar;
+    outputLuasPpanjang = panjangLuas * lebarLuas;
+}
+
+const kelilingPersegiPanjang = () => {
+    panjangKeliling = getValueFromInputs('kelilingPersegiPanjang').valPanjang;
+    lebarKeliling = getValueFromInputs('kelilingPersegiPanjang').valLebar;
+    outputKelilingPpanjang = 2 * panjangKeliling + 2 * lebarKeliling;
+}
+
+const hitungLuasPersegiPanjang = (e) => {
+    const output = e.previousElementSibling;
+    const outputTag = `
+    <p>L = ${panjangLuas} x ${lebarLuas}</p>
+    <p>L = ${outputLuasPpanjang}</p>
+    `;
+
+    output.innerHTML = outputTag;
+}
+
+const hitungKelilingPersegiPanjang = (e) => {
+    const output = e.previousElementSibling;
+    const outputTag = `
+    <p>K = 2 x (${panjangKeliling} + ${lebarKeliling})</p>
+    <p>K = ${outputKelilingPpanjang}</p>
+    `;
+
+    output.innerHTML = outputTag;
+}
+
+const resetAll = (e) => {
+    e.closest('form').querySelector('.output').innerHTML = "";
+}
